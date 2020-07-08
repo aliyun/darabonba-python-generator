807b49b762a3ac6f297186deade1914061bb56f8
wb-hx510875 <wb-hx510875@alibaba-inc.com>
2020-07-08 16:10:34 +0800

    test : add test case

--- tests/lib.tests.js
+++ tests/lib.tests.js
@@ -11,10 +11,14 @@ const {
   _camelCase,
   _subModelName,
   _string,
-  _upperFirst
+  _upperFirst,
+  _config,
+  _avoidKeywords,
+  _modify,
+  _symbol
 } = require('../src/lib/helper');
 
-describe('lib tests', function () {
+describe('debug should be ok', function () {
   beforeEach(function () {
     this.sinon.stub(console, 'log');
     this.sinon.stub(process, 'exit');
@@ -24,7 +28,7 @@ describe('lib tests', function () {
     this.sinon.stub(fs, 'existsSync');
   });
 
-  it('debug.dump should be ok', function () {
+  it('dump should be ok', function () {
     debug.dump(null, 1, '1', 1.11111);
     expect(console.log.calledWith('foo')).to.be.false;
     expect(console.log.calledWith(null)).to.be.true;
@@ -34,7 +38,7 @@ describe('lib tests', function () {
     expect(process.exit.calledWith(-1)).to.be.false;
   });
 
-  it('debug.halt should be ok', function () {
+  it('halt should be ok', function () {
     debug.halt(null, 1, '1', 1.11111);
     expect(console.log.calledWith('foo')).to.be.false;
     expect(console.log.calledWith(null)).to.be.true;
@@ -44,7 +48,7 @@ describe('lib tests', function () {
     expect(process.exit.calledWith(-1)).to.be.true;
   });
 
-  it('debug.jump should be ok', function () {
+  it('jump should be ok', function () {
     for (let i = 0; i < 3; i++) {
       debug.jump(1, `jump output : ${i}`);
     }
@@ -54,7 +58,7 @@ describe('lib tests', function () {
     expect(process.exit.calledWith(-1)).to.be.true;
   });
 
-  it('debug.stack should be ok', function () {
+  it('stack should be ok', function () {
     try {
       debug.stack('called debug stack with message', 'data1', 'data2');
     } catch (e) {
@@ -75,25 +79,36 @@ describe('lib tests', function () {
       expect(process.exit.calledWith(-1)).to.be.false;
     }
   });
+});
 
-  it('emitter.indent should be ok', function () {
+describe('emitter should be ok', function () {
+  beforeEach(function () {
+    this.sinon.stub(console, 'log');
+    this.sinon.stub(process, 'exit');
+    this.sinon.stub(fs, 'mkdirSync');
+    this.sinon.stub(fs, 'writeFileSync');
+    this.sinon.stub(fs, 'appendFileSync');
+    this.sinon.stub(fs, 'existsSync');
+  });
+
+  it('indent should be ok', function () {
     const emitter = new Emitter();
     expect(emitter.indent(1)).to.be.eql('    ');
   });
 
-  it('emitter.emit should be ok', function () {
+  it('emit should be ok', function () {
     const emitter = new Emitter();
     emitter.emit('emit some string');
     expect(emitter.output).to.be.eql('emit some string');
   });
 
-  it('emitter.emitln should be ok', function () {
+  it('emitln should be ok', function () {
     const emitter = new Emitter();
     emitter.emitln('emitln some string');
     expect(emitter.output).to.be.eql('emitln some string' + emitter.eol);
   });
 
-  it('emitter.emits should be ok', function () {
+  it('emits should be ok', function () {
     const emitter = new Emitter();
     emitter.emits(1);
     expect(emitter.output).to.be.eql('');
@@ -101,14 +116,14 @@ describe('lib tests', function () {
     expect(emitter.output).to.be.eql('row1' + emitter.eol + 'row2' + emitter.eol);
   });
 
-  it('emitter.erase should be ok', function () {
+  it('erase should be ok', function () {
     const emitter = new Emitter();
     emitter.emit('full string');
     emitter.erase(3);
     expect(emitter.output).to.be.eql('full str');
   });
 
-  it('emitter.savePath should be ok', function () {
+  it('savePath should be ok', function () {
     const emitter = new Emitter({
       dir: '/tmp/',
       layer: 'a.b.c',
@@ -118,7 +133,7 @@ describe('lib tests', function () {
     expect(emitter.savePath()).to.be.eql('/tmp/a/b/c/filename.tmp');
   });
 
-  it('emitter.save should be ok', function () {
+  it('save should be ok', function () {
     const emitter = new Emitter();
     try {
       emitter.save();
@@ -138,7 +153,7 @@ describe('lib tests', function () {
 
     emitter.save();
     emitter.emit();
-    mm(fs, 'existsSync', function(filename) {
+    mm(fs, 'existsSync', function (filename) {
       return true;
     });
     emitter.save(true);
@@ -152,23 +167,54 @@ describe('lib tests', function () {
 
     expect(filename).to.be.eql('/tmp/filename.tmp');
   });
+});
 
-  it('helper._upperFirst should be ok', function () { 
+describe('helper tests', function () {
+  it('_upperFirst should be ok', function () {
     expect(_upperFirst(null)).to.be.eql('');
   });
 
-  it('helper._camelCase should be ok', function () {
+  it('_camelCase should be ok', function () {
     expect(_camelCase('test_camel_case')).to.be.eql('testCamelCase');
   });
 
-  it('helper._subModelName should be ok', function () {
+  it('_subModelName should be ok', function () {
     expect(_subModelName('test.model.name')).to.be.eql('TestModelName');
   });
 
-  it('helper._string should be ok', function () {
+  it('_string should be ok', function () {
     const obj = {
       string: 'test'
     };
     expect(_string(obj)).to.be.eql('test');
+  });
+
+  it('_avoidKeywords should be ok', function () {
+    _config({ keywords: ['key'] });
+    expect(_avoidKeywords('key')).to.be.eql('key_');
+  });
+
+  it('_modify should be ok', function () {
+    _config({
+      modifyOrder: [
+        'PRIVATE',
+        'PROTECTED',
+        'PUBLIC',
+        'FINAL',
+        'ABSTRACT',
+        'STATIC'
+      ]
+    });
+    expect(_modify('PRIVATE')).to.be.eql('private');
+    expect(_modify(['PRIVATE', 'STATIC'])).to.be.eql('private static');
+  });
+
+  it('_symbol should be ok', function () {
+    _config({ symbolMap: { 'ASSIGN': '=' } });
+    expect(function () {
+      _symbol('InvalidSymbol');
+    }).to.be.throw('Unsupported symbol : InvalidSymbol');
+
+    expect(_symbol('ASSIGN')).to.be.eql('=');
   });
 });
