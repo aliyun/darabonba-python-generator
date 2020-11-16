40ad080457a17cf129548234888c65db1ed695a4
824750130 <e824750130@qq.com>
2020-11-16 17:39:39 +0800

    Fix aliases that don`t conform to python syntax

--- tests/expected/alias/tea_python_tests/client.py
+++ tests/expected/alias/tea_python_tests/client.py
@@ -5,6 +5,8 @@ from Alias.alias_client import AliasClient
 from Import.client import Client as ImportClient
 from Source.client import Client as SourceClient
 from alias_source.source_client import SourceClient as AliasSourceSourceClient
+from alias_symbol.client import Client as AliasSymbolClient
+from alias_symbol import models as alias_symbol_models
 
 
 class Client(object):
@@ -17,3 +19,5 @@ class Client(object):
         SourceClient.test()
         AliasClient.test()
         AliasSourceSourceClient.test()
+        AliasSymbolClient.test()
+        model = alias_symbol_models.TestModel()
